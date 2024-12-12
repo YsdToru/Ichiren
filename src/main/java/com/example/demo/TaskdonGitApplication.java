dec0c007ecc3dfa14f27f696745729a0ab7048c9
jacko <jacko@10.24.108.33>
2024-12-12 10:11:07 +0900

    初回コミット

---
+++ src/main/java/com/example/demo/TaskdonGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdonGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskdonGitApplication.class, args);
+	}
+
+}
